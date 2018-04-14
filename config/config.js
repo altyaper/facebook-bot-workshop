cf3398261bce6d442e9267f70e21746a31d90ad5
Jorge Chavez <jchavez@switchfly.com>
2018-04-14 03:30:04 +0000

    test

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ module.exports = {
   LUIS: {
     APPID: process.env.LUIS_APP_ID || "replace_here",
     APPSECRET: process.env.LUIS_APP_SECRET || "replace_here"
-  },  
-  FACEBOOK: {
-    VALIDATION_TOKEN: process.env.VALIDATION_TOKEN || 'this_is_my_token_ma_friend'
+  },
+  TELEGRAM: {
+    TOKEN: "569919480:AAGZS1x8927HIaCo79yKjFkFpJ6DttYWYmw"
   }
 }
--- routes/bot.js
+++ routes/bot.js
@@ -2,19 +2,11 @@ const express = require('express');
 const router = express.Router();
 const config = require('../config/config');
 
-router.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FACEBOOK.VALIDATION_TOKEN) {
-   res.status(200).send(req.query['hub.challenge']);
-  } else {
-   res.sendStatus(403);
-  }
-});
+router.post('/message', (req, res) => {
+  console.log("something llego");
+  console.log(req.body);
+  res.sendStatus(203);
 
-router.post('/webhook', (req, res) => {
-  let data = req.body;
-  console.log(data);
-  res.sendStatus(200);
 });
-
 
 module.exports = router;
