611751d2f0fc69570f2e5aa0e8d349c1b5ffcb85
dieggger <dieggger@gmail.com>
2018-04-14 01:09:28 -0600

    Code cleaning and refactors part II

--- routes/bot.js
+++ routes/bot.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const config = require('../config/config');
 const LUISClient = require("luis-node-sdk");
 const Smooch = require('smooch-core');
+
+const greetText = "Hola, mi nombre es Fábula, soy una robot que necesita entrenamiento.";
+const farewellText = "¡Fue un placer platicar contigo, adiós!.";
+const noneText = "Me gustaría ser humano para poder entenderte, intenta ser más específico.";
+const infoText = "Del 14 de abril al 28 de mayo, tendremos funciones los jueves a las 8pm, a un costo de 200 pesos";
+
 const smooch = new Smooch({
   keyId: config.SMOOCH.KEYID,
   secret: config.SMOOCH.SECRET,
   scope: 'app'
 });
 
-let LUIS = LUISClient({
+const LUIS = LUISClient({
   appId: config.LUIS.APPID,
   appKey: config.LUIS.APPSECRET,
   verbose: true
@@ -19,7 +25,7 @@ let appUserId;
 
 router.post('/message', (req, res) => {
   appUserId = req.body.appUser._id;
-  const text = req.body.messages[0].text;
+  const { text } = req.body.messages[0];
 
   console.log('Received message was:', text);
 
@@ -27,43 +33,38 @@ router.post('/message', (req, res) => {
     onSuccess: (data) => successPredict(data, res),
     onFailure: (data) => failurePredict(data, res)
   });
-
-  function failurePredict(err, res) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-
-  function successPredict(data, res) {
-    const { intent } = data.topScoringIntent;
-    console.log('Top intent:', intent);
-    console.log('Whole response:', data);
-    console.log('Top scoring:', data.topScoringIntent);
-
-    const sendMessage = (text) => (
-      smooch.appUsers.sendMessage(appUserId, { type: 'text', text, role: 'appMaker'})
-        .then(response => res.end())
-        .catch(err => res.end())
-    );
-
-    const greetText = "Hola, mi nombre es Fábula, soy una robot que necesita entrenamiento.";
-    const farewellText = "¡Fue un placer platicar contigo, adiós!.";
-    const noneText = "Me gustaría ser humano para poder entenderte, intenta ser más específico.";
-    const infoText = "Del 14 de abril al 28 de mayo, tendremos funciones los jueves a las 8pm, a un costo de 200 pesos";
-
-    switch (intent) {
-      case "Saludo":
-        sendMessage(greetText);
-        break;
-      case "Despedida":
-        sendMessage(farewellText);
-        break;
-      case "Eventos":
-        sendMessage(infoText);
-        break;
-      default:
-        sendMessage(noneText);
-    }
-  }
 });
+
+function failurePredict(err, res) {
+  console.error(err);
+  res.sendStatus(500);
+}
+
+function successPredict(data, res) {
+  const {intent} = data.topScoringIntent;
+  console.log('Top intent:', intent);
+  console.log('Whole response:', data);
+  console.log('Top scoring:', data.topScoringIntent);
+
+  const sendMessage = (text) => (
+    smooch.appUsers.sendMessage(appUserId, { type: 'text', text, role: 'appMaker' })
+      .then(response => res.end())
+      .catch(err => res.end())
+  );
+
+  switch (intent) {
+    case "Saludo":
+      sendMessage(greetText);
+      break;
+    case "Despedida":
+      sendMessage(farewellText);
+      break;
+    case "Eventos":
+      sendMessage(infoText);
+      break;
+    default:
+      sendMessage(noneText);
+  }
+}
 
 module.exports = router;
